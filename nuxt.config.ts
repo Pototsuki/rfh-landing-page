6a49b50db849b59e7506cdb503ea1019003d4c8a
Ricky Ng <ricky@aptika.co.id>
2026-01-08 11:20:43 +0700

    add starter template and deployment script

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+  modules: ["@nuxt/eslint", "@nuxt/ui", "@nuxt/a11y", "@nuxt/image"],
 
-  modules: [
-    '@nuxt/image',
-    '@nuxt/ui',
-    '@nuxt/a11y',
-    '@nuxt/eslint'
-  ]
-})
+  devtools: {
+    enabled: true,
+  },
+
+  ssr: true,
+
+  routeRules: {
+    "/": { prerender: true, static: true },
+    "/classes": { static: true },
+
+    "/classes/**": { ssr: true },
+  },
+
+  css: ["~/assets/css/main.css"],
+
+  compatibilityDate: "2025-01-15",
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: "never",
+        braceStyle: "1tbs",
+      },
+    },
+  },
+});
