e0633d34217f5f148f2a731be212d5ea88f50a0f
Ricky Ng <ricky@aptika.co.id>
2026-01-19 14:26:53 +0700

    add nuxt i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,29 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/eslint", "@nuxt/ui", "@nuxt/a11y", "@nuxt/image"],
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/ui",
+    "@nuxt/a11y",
+    "@nuxt/image",
+    "@nuxtjs/i18n",
+    "@nuxtjs/seo",
+  ],
+
+  ssr: true,
 
   devtools: {
     enabled: true,
   },
 
-  ssr: true,
+  css: ["~/assets/css/main.css"],
 
   routeRules: {
     "/": { prerender: true, static: true },
     "/classes": { static: true },
-
     "/classes/**": { ssr: true },
   },
-
-  css: ["~/assets/css/main.css"],
-
   compatibilityDate: "2025-01-15",
 
   eslint: {
     config: {
       stylistic: {
-        commaDangle: "never",
-        braceStyle: "1tbs",
+        commaDangle: "always",
       },
     },
   },
